18686bfaa9fd456f7c595bf748d3deab434e16e1
Ngoualem Ariel <ngoualemariel1@gmail.com>
2023-05-11 16:40:59 +0200

    react_calculatrice

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculatrice from './calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculatrice />
+    </div>
+  );
+}
+
+export default App;
